373a9cf5ba951e0b3af2416a2abb7279d4d8ed60
John P <john.pickett@gmail.com>
2019-03-10 19:40:57 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'new-acronyms';
+    constructor(private http:HttpClient) {}
+    title = 'MCG Acronyms';
+    acronyms = [];
+
+    loadAcronymData() {
+        console.log('loading data')
+        this.getAcroData().subscribe(
+            data => { this.acronyms = data },
+            err => console.error(err),
+            () => console.log('done loading acronyms')
+        )
+    }
+
+    getAcroData() {
+        return this.http.get(process.env.API_URL)
+    }
+
+    ngOnInit() {
+        console.log('starting up');
+        this.loadAcronymData()
+    }
 }
